09a28c277145513857cdc2889f3cd86af35e8082
peitos <andryhoy@gmail.com>
2022-05-04 22:52:29 +0300

    Добавил возможность нажатия на день, скрывает весь рабочий интерфейс

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,17 +1,14 @@
 package sample;
 
-import java.io.IOException;
 import java.util.*;
+
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
 import javafx.geometry.HPos;
-import javafx.geometry.VPos;
 import javafx.scene.Node;
-import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.control.TextArea;
 import javafx.scene.effect.GaussianBlur;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.paint.Color;
@@ -26,7 +23,25 @@ public class Controller {
     private Label currMonth;
 
     @FXML
+    private ImageView exitButton;
+
+    @FXML
+    private AnchorPane anchorPane;
+
+    @FXML
     private GridPane gridPane;
+
+    @FXML
+    private ImageView backgroundImage;
+
+    @FXML
+    private Label daysOfWeekLabel;
+
+    @FXML
+    private ImageView nextMonthButton;
+
+    @FXML
+    private ImageView prevMonthButton;
 
     public void initialize() {
         month = Calendar.getInstance().get(Calendar.MONTH);
@@ -60,7 +75,7 @@ public class Controller {
         Map<Integer, Integer> dayToMonthBefore = new HashMap<>();
         Calendar maxDays = new GregorianCalendar(year, month - 1, 1);
         int days = maxDays.getActualMaximum(Calendar.DAY_OF_MONTH);
-        for(int i = 0; i < dayToMonth.get(1); i++) {
+        for (int i = 0; i < dayToMonth.get(1); i++) {
             Calendar date = new GregorianCalendar(year, month - 1, days - i);
             int rightDay = (date.get(Calendar.DAY_OF_WEEK) + 5) % 7;
             dayToMonthBefore.put(date.get(Calendar.DAY_OF_MONTH), rightDay);
@@ -72,7 +87,7 @@ public class Controller {
         Map<Integer, Integer> dayToMonthAfter = new HashMap<>();
         Calendar maxDays = new GregorianCalendar(year, month, 1);
         int days = maxDays.getActualMaximum(Calendar.DAY_OF_MONTH);
-        for(int day = 1; day < 43 - days - dayToMonth.get(1); day++) {
+        for (int day = 1; day < 43 - days - dayToMonth.get(1); day++) {
             Calendar date = new GregorianCalendar(year, month + 1, day);
             int rightDay = (date.get(Calendar.DAY_OF_WEEK) + 5) % 7;
             dayToMonthAfter.put(date.get(Calendar.DAY_OF_MONTH), rightDay);
@@ -82,10 +97,10 @@ public class Controller {
 
     public int addPanel(Map<Integer, Integer> days, int week, boolean clickable) {
         Set<Integer> setKeys = days.keySet();
-        if(clickable){
+        if (clickable) {
             for (Integer k : setKeys) {
                 int dayOfWeek = days.get(k);
-                if(dayOfWeek == 5 || dayOfWeek == 6) {
+                if (dayOfWeek == 5 || dayOfWeek == 6) {
                     addButtons(k, days.get(k), week, "red");
                 } else {
                     addButtons(k, days.get(k), week, "white");
@@ -98,7 +113,7 @@ public class Controller {
                 Text text = new Text(String.valueOf(k));
                 text.setStyle("-fx-font-size: 25px");
                 GridPane.setHalignment(text, HPos.CENTER);
-                if(dayOfWeek == 5 || dayOfWeek == 6) {
+                if (dayOfWeek == 5 || dayOfWeek == 6) {
                     text.setFill(Color.CORAL);
                 } else {
                     text.setFill(Color.GRAY);
@@ -117,7 +132,7 @@ public class Controller {
         int week = 0;
         week = addPanel(dayToMonthBefore, week, false);
         week = addPanel(currMonth, week, true);
-        addPanel(dayToMonthAfter, week,false);
+        addPanel(dayToMonthAfter, week, false);
     }
 
     public void toLowerMonth() {
@@ -149,75 +164,34 @@ public class Controller {
         GridPane.setConstraints(button, col, row);
         gridPane.getChildren().add(button);
         button.setOnAction(e -> {
-            cellSelected(currentDate);
+            cellSelected();
             System.out.println(currentDate);
         });
     }
 
-    public void cellSelected(String currentDate) {
-        clearGridPane();
-        gridPane.setEffect(new GaussianBlur());
+    public void cellSelected() {
+        blur(true, backgroundImage, gridPane, daysOfWeekLabel, nextMonthButton, prevMonthButton);
+        exitButton.setVisible(true);
         chosenDate();
-//        AnchorPane newPane = new AnchorPane();
-//        root.setTop(newPane);
-        Button buttonBack = createBackButton();
-        buttonBack.setEffect(null);
-//        TextArea notes = createNotesArea();
-//        Text date = createDateText(currentDate);
-//        Text textNote = createNoteText();
-//        buttonBack.setOnAction(t -> {
-//            gridPane.getChildren().remove(buttonBack);
-//            gridPane.getChildren().remove(date);
-//            gridPane.getChildren().remove(notes);
-//            gridPane.getChildren().remove(textNote);
-//            fillCalendar();
-//        });
-        buttonBack.setOnAction(t -> {
-            gridPane.setEffect(null);
+        exitButton.setOnMouseClicked(t -> {
+            blur(false, backgroundImage, gridPane, daysOfWeekLabel, nextMonthButton, prevMonthButton);
             clearGridPane();
             fillCalendar();
+            exitButton.setVisible(false);
         });
     }
 
-    private Button createBackButton(){
-        Button buttonBack = new Button("X");
-        buttonBack.setMaxWidth(10);
-        buttonBack.setMaxHeight(10);
-        buttonBack.setStyle("-fx-background-color: red;");
-        GridPane.setConstraints(buttonBack, 6, 0);
-        GridPane.setHalignment(buttonBack, HPos.RIGHT);
-        GridPane.setValignment(buttonBack, VPos.TOP);
-        gridPane.getChildren().add(buttonBack);
-        return buttonBack;
+    public void blur(boolean blur, Node... objects) {
+        if(blur){
+            Arrays.stream(objects).forEach(obj -> obj.setEffect(new GaussianBlur()));
+            Arrays.stream(objects).forEach(obj -> obj.setDisable(blur));
+        } else {
+            Arrays.stream(objects).forEach(obj -> obj.setEffect(null));
+            Arrays.stream(objects).forEach(obj -> obj.setDisable(blur));
+        }
     }
 
-    private void chosenDate(){
+    private void chosenDate() {
 
     }
-
-//    private Text createDateText(String currentDate){
-//        Text date = new Text(currentDate);
-//        date.setStyle("-fx-font-size: 50;");
-//        date.setFill(Color.WHITE);
-//        GridPane.setValignment(date, VPos.CENTER);
-//        gridPane.setConstraints(date, 0, 0);
-//        gridPane.getChildren().add(date);
-//        return date;
-//    }
-//    private TextArea createNotesArea() {
-//        TextArea textArea = new TextArea();
-//        gridPane.setConstraints(textArea, 4, 3, 4, 5);
-//        gridPane.getChildren().add(textArea);
-//        return textArea;
-//    }
-//
-//    private Text createNoteText(){
-//        Text textNote = new Text("Заметки");
-//        textNote.setStyle("-fx-font-size: 45;");
-//        textNote.setFill(Color.WHITE);
-//        GridPane.setHalignment(textNote, HPos.CENTER);
-//        gridPane.setConstraints(textNote, 3, 1);
-//        gridPane.getChildren().add(textNote);
-//        return textNote;
-//    }
 }
