f95c06690e53a91f54c4fd656a944defa7e5eeec
podkydish <aaandreyev007@gmail.com>
2022-05-04 21:31:21 +0300

    Выделение текущего дня

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,7 +1,10 @@
 package sample;
 
 import java.io.IOException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.*;
+
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.geometry.HPos;
@@ -17,7 +20,7 @@ import javafx.scene.paint.Color;
 import javafx.scene.text.Text;
 
 public class Controller {
-
+    public static Calendar today = new GregorianCalendar();
     public static int month;
     public static int year;
 
@@ -59,7 +62,7 @@ public class Controller {
         Map<Integer, Integer> dayToMonthBefore = new HashMap<>();
         Calendar maxDays = new GregorianCalendar(year, month - 1, 1);
         int days = maxDays.getActualMaximum(Calendar.DAY_OF_MONTH);
-        for(int i = 0; i < dayToMonth.get(1); i++) {
+        for (int i = 0; i < dayToMonth.get(1); i++) {
             Calendar date = new GregorianCalendar(year, month - 1, days - i);
             int rightDay = (date.get(Calendar.DAY_OF_WEEK) + 5) % 7;
             dayToMonthBefore.put(date.get(Calendar.DAY_OF_MONTH), rightDay);
@@ -71,7 +74,7 @@ public class Controller {
         Map<Integer, Integer> dayToMonthAfter = new HashMap<>();
         Calendar maxDays = new GregorianCalendar(year, month, 1);
         int days = maxDays.getActualMaximum(Calendar.DAY_OF_MONTH);
-        for(int day = 1; day < 43 - days - dayToMonth.get(1); day++) {
+        for (int day = 1; day < 43 - days - dayToMonth.get(1); day++) {
             Calendar date = new GregorianCalendar(year, month + 1, day);
             int rightDay = (date.get(Calendar.DAY_OF_WEEK) + 5) % 7;
             dayToMonthAfter.put(date.get(Calendar.DAY_OF_MONTH), rightDay);
@@ -81,13 +84,19 @@ public class Controller {
 
     public int addPanel(Map<Integer, Integer> days, int week, boolean clickable) {
         Set<Integer> setKeys = days.keySet();
-        if(clickable){
+
+
+        if (clickable) {
             for (Integer k : setKeys) {
                 int dayOfWeek = days.get(k);
-                if(dayOfWeek == 5 || dayOfWeek == 6) {
-                    addButtons(k, days.get(k), week, "red");
+                if (dayOfWeek == 5 || dayOfWeek == 6) {
+                    addButtons(k, days.get(k), week, "-fx-text-fill: red;", "");
+
                 } else {
-                    addButtons(k, days.get(k), week, "white");
+                    addButtons(k, days.get(k), week, "-fx-text-fill: white;", "");
+                }
+                if (today.get(Calendar.DAY_OF_MONTH) == k && today.get(Calendar.MONTH) == month && today.get(Calendar.YEAR) == year) {
+                    addButtons(k, days.get(k), week, "-fx-text-fill: transparent;", "; -fx-border-color: red; -fx-border-width: 2px");
                 }
                 if (dayOfWeek == 6) week++;
             }
@@ -97,7 +106,7 @@ public class Controller {
                 Text text = new Text(String.valueOf(k));
                 text.setStyle("-fx-font-size: 25px");
                 GridPane.setHalignment(text, HPos.CENTER);
-                if(dayOfWeek == 5 || dayOfWeek == 6) {
+                if (dayOfWeek == 5 || dayOfWeek == 6) {
                     text.setFill(Color.CORAL);
                 } else {
                     text.setFill(Color.GRAY);
@@ -109,6 +118,7 @@ public class Controller {
         return week;
     }
 
+
     public void fillCalendar() {
         Map<Integer, Integer> currMonth = getCurrMonth();
         Map<Integer, Integer> dayToMonthBefore = getDaysBefore(currMonth);
@@ -116,7 +126,7 @@ public class Controller {
         int week = 0;
         week = addPanel(dayToMonthBefore, week, false);
         week = addPanel(currMonth, week, true);
-        addPanel(dayToMonthAfter, week,false);
+        addPanel(dayToMonthAfter, week, false);
     }
 
     public void toLowerMonth() {
@@ -138,20 +148,28 @@ public class Controller {
     }
 
 
-    public void addButtons(int day, int col, int row, String color) {
-        String currentDate = String.format("%02d:%02d:%d", day, month, year);
+    public void addButtons(int day, int col, int row, String color, String border) {
+        String currentDate = String.format("%02d.%02d.%d", day, month, year);
         Button button = new Button(String.valueOf(day));
         button.setMaxWidth(Double.MAX_VALUE);
         button.setMaxHeight(Double.MAX_VALUE);
-        button.setStyle("-fx-background-color: transparent; -fx-text-fill: " + color + "; -fx-font-size: 25px");
+        button.setStyle("-fx-background-color: transparent; " + color + " -fx-font-size: 25px" + border);
         button.setId(currentDate);
         GridPane.setConstraints(button, col, row);
         gridPane.getChildren().add(button);
         button.setOnAction(e -> {
-            cellSelected(currentDate);
             System.out.println(currentDate);
+            Scene scene2 = null;
+            try {
+                scene2 = new Scene(FXMLLoader.load(getClass().getResource("sample2.fxml")));
+            } catch (IOException a) {
+                a.printStackTrace();//переписать
+            }
+            Scene finalScene = scene2;
+            Main.switchScenes(finalScene);
         });
     }
+
 
     public void cellSelected(String currentDate) {
         clearGridPane();
@@ -181,7 +199,7 @@ public class Controller {
         });
     }
 
-    private Button createBackButton(){
+    private Button createBackButton() {
         Button buttonBack = new Button("X");
         buttonBack.setMaxWidth(10);
         buttonBack.setMaxHeight(10);
@@ -193,7 +211,7 @@ public class Controller {
         return buttonBack;
     }
 
-    private Text createDateText(String currentDate){
+    private Text createDateText(String currentDate) {
         Text date = new Text(currentDate);
         date.setStyle("-fx-font-size: 50;");
         date.setFill(Color.WHITE);
@@ -202,6 +220,7 @@ public class Controller {
         gridPane.getChildren().add(date);
         return date;
     }
+
     private TextArea createNotesArea() {
         TextArea textArea = new TextArea();
         gridPane.setConstraints(textArea, 4, 3, 4, 5);
@@ -209,7 +228,7 @@ public class Controller {
         return textArea;
     }
 
-    private Text createNoteText(){
+    private Text createNoteText() {
         Text textNote = new Text("Заметки");
         textNote.setStyle("-fx-font-size: 45;");
         textNote.setFill(Color.WHITE);
@@ -219,3 +238,5 @@ public class Controller {
         return textNote;
     }
 }
+
+//внедриться в getcurrmonth
