549e31a242ca3e2e977eb00c35a16862122ef900
peitos <andryhoy@gmail.com>
2022-05-13 18:47:09 +0300

    Комментарии + чуть исправил методы

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,8 +1,7 @@
 package sample;
 
-import java.time.format.DateTimeFormatter;
-import java.time.format.DateTimeParseException;
-import java.time.format.ResolverStyle;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.*;
 
 import javafx.fxml.FXML;
@@ -21,9 +20,14 @@ import javafx.scene.paint.Color;
 import javafx.scene.text.Text;
 
 public class Controller {
-    public static Calendar today = new GregorianCalendar();
-    public static int month;
-    public static int year;
+
+    /**
+     * Объявление переменных
+     */
+
+    private static final Calendar today = new GregorianCalendar();
+    private static int month;
+    private static int year;
     private static Map<Integer, String> currMonthForecast = new HashMap<>();
 
     @FXML
@@ -56,6 +60,9 @@ public class Controller {
     @FXML
     private Button buttonSave;
 
+    /**
+     * Выполняется при запуске, задаёт текущие месяц и год в актуальные
+     */
 
     public void initialize() {
         month = Calendar.getInstance().get(Calendar.MONTH);
@@ -63,6 +70,13 @@ public class Controller {
         fillCalendar();
     }
 
+    /**
+     * Выводит надпись в левый верхний угол о текущем месяце и годе,
+     * назначает каждому дню свой день недели
+     *
+     * @return Мапу, где ключ - день месяца, значение - день недели, который он занимает
+     */
+    // создаёт надпись в левом верхнем углу, распределяет дни месяца по дням недели
     public Map<Integer, Integer> getCurrMonth() {
         updateYear();
         currMonth.setText(Months.values()[month].toString() + " " + year);
@@ -78,6 +92,12 @@ public class Controller {
         return dayToMonth;
     }
 
+    /**
+     * Генерирует месяц, идущий до текущего, чтобы отобразить последние даты
+     *
+     * @param dayToMonth Мапа дней текущего месяца, с учтенными днями недели
+     * @return Мапу последних дат предыдущего месяца
+     */
     public Map<Integer, Integer> getDaysBefore(Map<Integer, Integer> dayToMonth) {
         updateYear();
         Map<Integer, Integer> dayToMonthBefore = new HashMap<>();
@@ -90,6 +110,13 @@ public class Controller {
         }
         return dayToMonthBefore;
     }
+
+    /**
+     * Генерирует месяц, идущий после текущего, чтобы отобразить первые даты
+     *
+     * @param dayToMonth Мапа дней текущего месяца, с учтенными днями недели
+     * @return Мапу первых дат последующего месяца
+     */
 
     public Map<Integer, Integer> getDaysAfter(Map<Integer, Integer> dayToMonth) {
         updateYear();
@@ -104,6 +131,10 @@ public class Controller {
         return dayToMonthAfter;
     }
 
+    /**
+     * Если месяц меньше или больше допустимого, меняет значение года
+     */
+
     public void updateYear() {
         if (month == 12) {
             month = 0;
@@ -115,12 +146,25 @@ public class Controller {
         }
     }
 
+    /**
+     * Задаёт текстуру в зависимости от погоды в этот день,
+     * если день текущего месяца, он будет кликабелен
+     * вызываем метод, устанавливающий текстуру на каждый день
+     *
+     * @param days      Мапа дней месяца, дни которого ставим в календарь
+     * @param week      Неделя, в которую нужно поставить данный день
+     * @param clickable Будет ли день являться кликабельным(текущий месяц)
+     * @return Номер недели месяца, на котором мы остановились
+     */
+
     public int addPanel(Map<Integer, Integer> days, int week, boolean clickable) {
         Set<Integer> setKeys = days.keySet();
         String blockType = "stone";
         if (clickable) {
             for (Integer k : setKeys) {
-                if (currMonthForecast.get(k - 1).toLowerCase().contains("снег")) {
+                if (currMonthForecast == null) {
+                    blockType = "earthblock";
+                } else if (currMonthForecast.get(k - 1).toLowerCase().contains("снег")) {
                     blockType = "snowblock";
                 } else if (currMonthForecast.get(k - 1).contains("осадки") || currMonthForecast.get(k - 1).contains("дождь")) {
                     blockType = "waterearthblock";
@@ -129,12 +173,12 @@ public class Controller {
                 }
                 int dayOfWeek = days.get(k);
                 if (dayOfWeek == 5 || dayOfWeek == 6) {
-                    addButtons(k, days.get(k), week, blockType, Color.CYAN, false);
+                    addButtons(k, days.get(k), week, blockType, Color.CYAN);
                 } else {
-                    addButtons(k, days.get(k), week, blockType, Color.WHITE, false);
+                    addButtons(k, days.get(k), week, blockType, Color.WHITE);
                 }
                 if (today.get(Calendar.DAY_OF_MONTH) == k && today.get(Calendar.MONTH) == month && today.get(Calendar.YEAR) == year) {
-                    addButtons(k, days.get(k), week, blockType, Color.LIME, true);
+                    addButtons(k, days.get(k), week, blockType, Color.LIME);
                 }
                 if (dayOfWeek == 6) week++;
             }
@@ -159,6 +203,12 @@ public class Controller {
         return week;
     }
 
+    /**
+     * Генерирует текущий месяц, предыдущий и следующий,
+     * получает погоду,
+     * меняет счетчик недели в зависимости от возвращенной,
+     * вызывает метод добавления дней каждого месяца в календарь.
+     */
 
     public void fillCalendar() {
         Map<Integer, Integer> currMonth = getCurrMonth();
@@ -171,11 +221,19 @@ public class Controller {
         addPanel(dayToMonthAfter, week, false);
     }
 
+    /**
+     * Меняет месяц на предыдущий и генерирует календарь заново
+     */
+
     public void toLowerMonth() {
         clearGridPane();
         --month;
         fillCalendar();
     }
+
+    /**
+     * Меняет месяц на следующий и генерирует календарь заново
+     */
 
     public void toHigherMonth() {
         clearGridPane();
@@ -183,24 +241,29 @@ public class Controller {
         fillCalendar();
     }
 
+    /**
+     * Поиск необходимого месяца и года
+     */
+
     public void search() {
-        try {
-            DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("M.uuuu").withResolverStyle(ResolverStyle.STRICT);
-            dateFormatter.parse(toSearch.getText());
-            String[] searchArray;
-            searchArray = toSearch.getText().split("[.]");
-            int searchMonth = Integer.parseInt(searchArray[0]);
-            int searchYear = Integer.parseInt(searchArray[1]);
+        String[] searchArray;
+        searchArray = toSearch.getText().split("[.]");
+        int searchMonth = Integer.parseInt(searchArray[0]);
+        int searchYear = Integer.parseInt(searchArray[1]);
+        if (searchMonth > 0 && searchMonth < 13) {
             month = searchMonth - 1;
             year = searchYear;
             clearGridPane();
             fillCalendar();
-        } catch (DateTimeParseException e) {
-            System.out.println("Date is not valid");
-        } finally {
-            toSearch.setText("");
+        } else {
+            System.out.println("Миссия невыполнима");
         }
+        toSearch.setText("");
     }
+
+    /**
+     * Очистка поля
+     */
 
     public void clearGridPane() {
         Node node = gridPane.getChildren().get(0);
@@ -208,13 +271,19 @@ public class Controller {
         gridPane.getChildren().add(0, node);
     }
 
+    /**
+     * Добавляет кликабельные картинки
+     *
+     * @param day       День
+     * @param col       Колонка в таблице
+     * @param row       Строка в таблице
+     * @param blockType Тип картинки
+     * @param color     Цвет цифры дня на картинке
+     */
 
-    public void addButtons(int day, int col, int row, String blockType, Color color, boolean border) {
+    public void addButtons(int day, int col, int row, String blockType, Color color) {
         String currentDate = String.format("%02d.%02d.%d", day, month + 1, year);
         ImageView button = new ImageView(new Image("images/" + blockType + ".jpg"));
-        if (border) {
-            button.setStyle("-fx-effect: dropshadow(three-pass-box, rgba(255,0,0,0.8), 10, 0, 0, 0);");
-        }
         Text date = new Text(String.valueOf(day));
         date.setStyle("-fx-font-size: 35px; -fx-stroke-color: black; -fx-stroke-width: 1px");
         GridPane.setHalignment(date, HPos.CENTER);
@@ -237,15 +306,21 @@ public class Controller {
         });
     }
 
+    /**
+     * Открываем меню дня, доступ к заметкам, запрещает нажимать кнопки
+     *
+     * @param buttonID Айди кнопки(дата)
+     */
+
     public void cellSelected(String buttonID) {
         blur(true, backgroundImage, gridPane, daysOfWeekLabel, nextMonthButton, prevMonthButton);
         exitButton.setVisible(true);
         textArea.setVisible(true);
         buttonSave.setVisible(true);
         blur(false, textArea);
-        textArea.setText(Files.searchNotesInFile("Notes", buttonID));
+        textArea.setText(FileWorker.searchNotesInFile(buttonID));
         buttonSave.setOnMouseClicked(e -> {
-            Files.addNotesToFile("Notes", buttonID, buttonID + "\n" + textArea.getText() + "\nEOT\n");
+            FileWorker.addNotesToFile(buttonID, "\n" + textArea.getText() + "\nEOT\n");
         });
         exitButton.setOnMouseClicked(t -> {
             textArea.setText("");
@@ -258,6 +333,13 @@ public class Controller {
         });
     }
 
+    /**
+     * Блюрит и разблюривает фон
+     *
+     * @param blur    Блюрить или разблюрить
+     * @param objects Объекты, которые нужно отключить
+     */
+
     public void blur(boolean blur, Node... objects) {
         if (blur) {
             Arrays.stream(objects).forEach(obj -> obj.setEffect(new GaussianBlur()));
@@ -268,9 +350,16 @@ public class Controller {
         }
     }
 
-    public void saveText(){
+    /**
+     * Очищает файл с погодой
+     */
 
+    public void clearForecast() {
+        try (FileWriter writer = new FileWriter("src/files/forecast.txt", false)) {
+            writer.write("");
+        } catch (IOException ex) {
+            System.out.println(ex.getMessage());
+        }
     }
 
 }
-//внедриться в getcurrmonth
